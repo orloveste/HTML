38ef17220a8ea24fd837bb31cd8db87df799ed60
orloveste <orlovevm@yahoo.com>
2020-02-29 13:49:32 +0200

    js run notFound

--- .idea/script.js
+++ .idea/script.js
@@ -1,1 +1,23 @@
 console.log('mama mia')
+var x = document.getElementById("SpecificElement");
+console.log(x)
+var y = document.getElementById("NewSpecific");
+console.log(y)
+var z = document.getElementsByClassName("fontSize30 newStyle");
+console.log(z)
+var demo = function () {
+    x+= 1;
+console.log('item '+ x +" something")
+}
+    //functie care afiseaza un element salvat in variabila
+x.addEventListener("click", demo);
+
+var demo1 = function () {
+    console.log("Do not do this - will be an error!")
+}
+
+
+var demo2 = function (a, b) {
+return a+b;
+}
+y.addEventListener("click", demo2)
