90eca96e330d002b86728ea24042e44214e47d91
orloveste <orlovevm@yahoo.com>
2020-03-01 13:38:32 +0200

    html - Vue - log in form - not done

--- .idea/script.js
+++ .idea/script.js
@@ -2,16 +2,21 @@ var app = new Vue({
     el: '#app',
     data: {
         myObject: {
-            name: ' ',
-            surname: '',
-            accountName: ''
+            accountName: '',
+            password: ' ',
 
-        }
+        },
+        todos: [
+            {textInvalidPass: 'invalid pass'},
+            {textValidPass: 'you are log in'}
+        ]
 
     },
     methods :{
         mySpecialFunction : function () {
-           window.alert('you clicked the button')
+           window.alert('you clicked the button'),
+               password = 1234
+
         },
         hoverFunction : function () {
             console.log('over the button')
