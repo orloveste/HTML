1b4ce289dbbdc7b275da048ba977bb831c2fcd07
orloveste <orlovevm@yahoo.com>
2020-03-01 10:09:46 +0200

    html - Vue - multiply message

--- .idea/script.js
+++ .idea/script.js
@@ -1,6 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        message2: ' second hello',
+        message3: ' New generation'
     }
 })
