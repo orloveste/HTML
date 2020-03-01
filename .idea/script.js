720d43d13e52745f3fd16b271729361b415e60b8
orloveste <orlovevm@yahoo.com>
2020-03-01 10:26:01 +0200

    html - Vue - mesage style

--- .idea/script.js
+++ .idea/script.js
@@ -1,6 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'You loaded this page on ' + new Date().toLocaleString()
+        message: 'You loaded this page on ' + new Date().toLocaleString(),
+        message2: ' second hello',
+        message3: ' New generation',
+        mesajStyle: 'color: red',
+        messageStyleBlue: 'color: blue'
     }
 })
