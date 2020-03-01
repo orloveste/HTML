58ced14a74e948c241248a54622052812e9a5ba8
orloveste <orlovevm@yahoo.com>
2020-03-01 11:14:18 +0200

    html - Vue - list with for take objects an pull in front end html

--- .idea/script.js
+++ .idea/script.js
@@ -1,12 +1,20 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'You loaded this page on ' + new Date().toLocaleString(),
-        message2: ' second hello',
-        message3: ' New generation',
-        mesajStyle: 'color: red',
-        messageStyleBlue: 'color: blue',
-        shouldAppear: true,
-        shouldantAppear: false
+        masini:[
+            {nameCar : 'Dacia'},
+            {nameCar : 'Opel'},
+            {nameCar : 'Fiat'},
+            {nameCar : 'Ford'},
+        ],
+        flowers:[
+            {flowerName: 'Zambila',
+                flowerColor: 'purple'},
+            {flowerName: 'Lily',
+                flowerColor: 'white'},
+            {flowerName: 'Roses',
+                flowerColor: 'red'},
+
+        ]
     }
 })
