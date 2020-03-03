fffc720bec4f0ef82b44079677fa522ed74a8107
orloveste <orlovevm@yahoo.com>
2020-03-03 09:53:47 +0200

    html - Vue -tema 1- noGo - testing list

--- .idea/script.js
+++ .idea/script.js
@@ -1,21 +1,7 @@
 
 Vue.component('merdeneaComponent', {
     props: ['merdenea'],
-    template: '<li>{{updateMessage}}</li>',//numele merdenelei de intrat aici din input
-    data: function () {//call back?
-        return{
-            merdenea: 'not updated'
-        }
-    },
-    metthods:{
-        updateMessage: async function () {
-            this.merdenea = 'updated'
-            console.log(this.$el.textContent)
-            await this.$nextTick(function () {
-                console.log(this.$el.textContent)
-            })
-        }
-    }
+    template: '<li>add new</li>',//numele merdenelei de intrat aici din input
 })
 var app = new Vue({
     el: '#app',
@@ -25,22 +11,17 @@ var app = new Vue({
         message3: ' New generation',
         mesajStyle: 'color: red',
         messageStyleBlue: 'color: blue',
-        message: 'new one',
         merdenea: {
             numeMerdenea: 'facatura',
         },
         merdenele: []
     },
-    //template: '<div>{{this.merdenea.numeMerdenea}}<\div>',
     methods :{
         addMerdenea : function () {
             this.merdenele.push(
                 {nume: this.merdenea.numeMerdenea},
             );
             //this.merdenea.numeMerdenea=''; //reset input
-        },
-        // addInList : function () {
-        //     this.addMerdenea()
-        // }
+        }
     }
 });
