7efbbb551105b175d3bcb9006b884b25cae05c8a
orloveste <orlovevm@yahoo.com>
2020-03-04 10:57:42 +0200

    html - Vue -  v-for directive can be used for displaying a list of items using the               data from an Array

--- .idea/script.js
+++ .idea/script.js
@@ -3,6 +3,27 @@ var app = new Vue({
     data: {
         message: 'You loaded this page on ' + new Date().toLocaleString(),
         seen: true,
-        unseen: false
+        unseen: false,
+        todos:[
+            {text: false},
+            {text: true},
+            {text: app},
+            {text: this},
+            {text: function () {
+                }},
+            // {text: new onmessage}, TypeError: onmessage is not a constructor
+            {text: null},
+            {text: typeof String},
+            {text: "symbol"},
+            // {text: async }, ReferenceError: async is not defined
+            {text: delete function () {
+
+                }},
+            // {text: import()}, SyntaxError: import() requires a specifier
+            {text: undefined},
+            {text: void this},
+
+
+        ]
     }
 });
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 "# HTML" 
+Vue
+apply transition effects when elements are inserted/updated/removed by Vue.
+https://vuejs.org/v2/guide/transitions.html
