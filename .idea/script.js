19a7089fe3cb7ff27d2f1a0337798d91e5b5e886
orloveste <orlovevm@yahoo.com>
2020-03-04 09:26:28 +0200

    html - Vue - message on browser - load date and time

--- .idea/script.js
+++ .idea/script.js
@@ -1,35 +1,6 @@
-
-Vue.component('merdeneaComponent', {
-    props: ['merdenele'],
-    // props: ['merdenea'],
-    template: '<li v-bind:nume></li>',//numele merdenelei de intrat aici din input
-})
-
 var app = new Vue({
     el: '#app',
     data: {
         message: 'You loaded this page on ' + new Date().toLocaleString(),
-        message2: ' second hello',
-        message3: ' New generation',
-        mesajStyle: 'color: red',
-        messageStyleBlue: 'color: blue',
-        seen: true,
-        merdenea: {
-            numeMerdenea: 'facatura',
-        },
-        merdenele: []
-    },
-    methods :{
-        addMerdenea : function () {
-            this.merdenele.push(
-                {nume: this.merdenea.numeMerdenea},
-            );
-            // this.merdenea.numeMerdenea=''; //reset input
-        },
     }
 });
-app.template = 'new message'
-app.$el.textContent === 'new message'
-Vue.nextTick(function () {
-    app.$el.textContent === 'new message'
-})
