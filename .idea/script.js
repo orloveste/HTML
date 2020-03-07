4861815e3d4176d8294587d2913ffad86a0f5c4b
orloveste <orlovevm@yahoo.com>
2020-03-07 10:56:22 +0200

    html- Vue -class ex1

--- .idea/script.js
+++ .idea/script.js
@@ -1,78 +1,30 @@
-Vue.component('merdeneaComponent', {
-    // props: ['merdenele'],
+Vue.component('productComponent', {
     props: ['baba'],
-    template: '<li>add new => {{baba.nume}}</li>',//numele merdenelei de intrat aici din input
+    template: '<li>add new => {{baba.id}} - {{baba.nume}}, {{baba.cantitate}} piece</li>',//numele merdenelei de intrat aici din input
 })
-Vue.component('todo-item',{
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
-})
-
-var data = {a:1}
-var vm = new Vue({
-    data:data
-})
-vm.a == data.a
-vm.a = 2
-data.a // => 2
-data.a = 3
-vm.a // => 3
 
 var app = new Vue({
     el: '#app',
 
     data: {
-        merdenea: {
-            idMerdenea: 0,
-            numeMerdenea: 'facatura',
+        product: {
+            idProduct: 0,
+            numeProduct: 'Water',
+            quantity:'1'
         },
-        merdenele: [],
-
-        count: 0,
-
-        groceryList:[
-            {id:0, text:'Milk'},
-            {id:1, text:'Eggs'},
-            {id:2, text:'Lava cake'}
-        ],
+        listProduct: [],
 
         message: 'You loaded this page on ' + new Date().toLocaleString(),
-        seen: true,
-        unseen: false,
-        todos:[
-            {text: false},
-            {text: true},
-            {text: app},
-            {text: this},
-            {text: function () {
-                }},
-            // {text: new onmessage}, TypeError: onmessage is not a constructor
-            {text: null},
-            {text: typeof String},
-            {text: "symbol"},
-            // {text: async }, ReferenceError: async is not defined
-            {text: delete function () {
-
-                }},
-            // {text: import()}, SyntaxError: import() requires a specifier
-            {text: undefined},
-            {text: void this},
-        ]
     },
     methods :{
-        addMerdenea : function () {
-            this.merdenele.push(
-                {id: this.merdenea.idMerdenea +=1,
-                nume: this.merdenea.numeMerdenea},
+        addProduct : function () {
+            this.listProduct.push(
+                {id: this.product.idProduct +=1,
+                nume: this.product.numeProduct,
+                cantitate: this.product.quantity}
             );
-            // this.merdenea.numeMerdenea=''; //reset input
+             // this.product.numeProduct=''; //reset input
         },
-        // incrementMerdenea: function(){
-        //     this.idMerdenea +=1;
-        // },
-        reverseMessage: function () {
-            this.message = this.message.split('').reverse().join('')
-        }
     }
 });
-app.todos.push({ text: 'New item' })
+
