a7c127fbce6c58a6f05dbf6217a72764e64f3aa8
orloveste <orlovevm@yahoo.com>
2020-03-08 13:00:33 +0200

    html- Vue -axios learn class - try to make array output

--- .idea/script.js
+++ .idea/script.js
@@ -1,17 +1,27 @@
+Vue.component('currencyComponent',{
+    props: ['babalak'],
+    template:'<li> {{baba.usdCode}} Rate : {{baba.usdRate}} {{baba.usdSymbol}}'
+})
+
+
+
 var app = new Vue({
     el: '#app',
     data () {
         return{
             info: null,
             update: null,
+
             usdCode: null,
             usdRate: null,
             usdSymbol: null,
+
             euroCode: null,
             euroRate: null,
             euroSymbol: null,
             statusCode: null,
         }
+        listCurrency: []
     },
     mounted () {
         var self  = this
@@ -21,6 +31,7 @@ var app = new Vue({
                 self.info = response.data.bpi;
                 self.update = response.data.time.updated;
                 self.statusCode = response.status;
+
                 self.usdCode = response.data.bpi.USD.code;
                 self.usdRate = response.data.bpi.USD.rate;
                 self.usdSymbol = response.data.bpi.USD.symbol;
@@ -29,6 +40,16 @@ var app = new Vue({
                 self.euroRate = response.data.bpi.EUR.rate;
                 self.euroSymbol = response.data.bpi.EUR.symbol;
             })
+    },
+    methods:{
+        addCurrency: function () {
+            this.listCurrency.push(
+                {usdCode: this.usdCode,
+                    usdRate: this.usdRate,
+                    usdSymbol: this.usdSymbol
+                }
+            )
+        }
     }
 })
 
