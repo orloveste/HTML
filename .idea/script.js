133a57f0eeea833a47bb54c32c5cbf390ae3218e
orloveste <orlovevm@yahoo.com>
2020-03-08 11:42:36 +0200

    html- Vue -axios learn class - info, undated, currency usd and euro

--- .idea/script.js
+++ .idea/script.js
@@ -2,13 +2,26 @@ var app = new Vue({
     el: '#app',
     data () {
         return{
-            info: null
+            info: null,
+            update: null,
+            usdCode: null,
+            usdRate: null,
+            euroCode: null,
+            euroRate: null,
         }
     },
     mounted () {
+        var self  = this
         axios
             .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-            .then(response => (this.info = response.data.bpi))
+            .then(function (response) {
+                self.info = response.data.bpi;
+                self.update = response.data.time.updated;
+                self.usdCode = response.data.bpi.USD.code;
+                self.usdRate = response.data.bpi.USD.rate;
+                self.euroCode = response.data.bpi.EUR.code;
+                self.euroRate = response.data.bpi.EUR.rate;
+            })
     }
 })
 
