4006b2ef579b2167c2b20319ed17146a3eb81d1a
orloveste <orlovevm@yahoo.com>
2020-03-08 11:05:15 +0200

    html- Vue -axios learn class - short data from api

--- .idea/script.js
+++ .idea/script.js
@@ -8,7 +8,7 @@ var app = new Vue({
     mounted () {
         axios
             .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-            .then(response => (this.info = response))
+            .then(response => (this.info = response.data.bpi))
     }
 })
 
